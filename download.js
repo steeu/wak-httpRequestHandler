42e456f8a2e99c19f92611db14ddfd2fb12daa8b
Stefan Eugster <stefan.eugster@gmail.com>
2015-08-03 16:53:28 +0200

    Create download.js

---
+++ download.js
@@ -0,0 +1,31 @@
+
+/**
+ * download httprequest handler
+ *
+ * @return {Blob} file
+ */
+
+function download(request, response) {
+	try {
+	    var file = File(ds.getDataFolder().path + request.urlPath),
+	    	blob;
+	    
+	    // validate file
+	    if (!file.exists) {
+	        response.statusCode = '404';
+	        return;
+	    }
+	    // add headers to force download
+		response.headers['Content-Type'] = 'application/octet-stream';
+	    response.headers['Content-Disposition'] = 'attachment; filename="' + file.name +'"';
+		// convert to blob
+		blob = file.toBuffer().toBlob();
+		// remove file from server
+		file.remove();
+			
+	    return blob;		
+	} catch (e) {
+		WAKTOOLS.log(e);
+		return e;		
+	}
+};
