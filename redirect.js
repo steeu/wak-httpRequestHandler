31ea0da8e01e3517a184a333482e228908b93ecd
Stefan Eugster <stefan.eugster@llv.li>
2015-08-05 07:56:59 +0200

    code

--- redirect.js
+++ redirect.js
@@ -6,17 +6,16 @@
 
 function redirectToHttps(request, response){
 	try {
-		// validate request
-		if (!request.isSSL && application.httpServer.ssl.enabled) { 
-			// if the current request is not ssl, but the server has ssl enabled
-			response.headers['Content-Type'] = 'text/html; charset=UTF-8';
-			response.statusCode = 307;
-
+		// validate if the current request is not ssl but the server has ssl enabled
+		if (!request.isSSL && application.httpServer.ssl.enabled) {
 			var port = application.httpServer.ssl.port;
 			var h = request.host.split(':');
 			var host = h[0];
 			var url = 'https://' + host + ':' + port + request.urlPath;
-
+			
+			// response
+			response.statusCode = 307;
+			response.headers['Content-Type'] = 'text/html; charset=UTF-8';
 			response.headers['Location'] = url;
 		}
 		
